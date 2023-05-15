9cee59e843540467776c3e13cd3d2024de59cab8
Ahmad Ainur Prambudia <116671785+AhmadAinurPrambudia@users.noreply.github.com>
2023-05-15 10:29:56 +0700

    Update reverse_sentence.py

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -1,1 +1,21 @@
+def reverse sentence(sentence):
 
+stack=[]
+
+reversed_sentence
+
+for word in sentence.split():
+
+stack.append(word)
+
+I while len(stack) > 0:
+
+reversed sentence + stack.pop()+"
+
+return reversed sentence.strip()
+
+sentence "Selamat pagi, bagaimana
+
+kabar Anda?"
+
+print(reverse_sentence(sentence))
